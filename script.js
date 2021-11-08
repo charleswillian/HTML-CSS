82b70ec1158aa4625b590b69f93f67d3c322d92d
Charles Willian <64935895+charleswillian@users.noreply.github.com>
2021-11-08 15:19:37 -0300

    Tela_Login

---
+++ script.js
@@ -0,0 +1,32 @@
+const init = () => {
+    const inputEmail = document.querySelector('input[type="email"]');
+
+    const inputPassword = document.querySelector('input[type="password"]');
+
+    const submitButton = document.querySelector('.loginSubmit');
+
+    if(submitButton){
+        submitButton.addEventListener('click', (Event) =>{
+            Event.preventDefault();
+
+            fetch('https://reqres.in/api/login', {
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: inputEmail.value,
+                    password: inputPassword.value,
+
+                })
+            }).then((Response)=> {
+                return Response.json();
+            }).then((data)=>{
+                console.log(data)
+            })
+        })
+    }
+
+}
+
+window.onload = init;
